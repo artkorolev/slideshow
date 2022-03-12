c8aa59785068e338eb63e27ce417f7fb1f6cae75
Artem Korolev <itemkorolev@gmail.com>
2022-03-12 18:56:30 +0200

    Change Slides Function

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+var slideIndex = 1;
+showSlides((slideIndex));
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName('slides');
+    let i;
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slides[slideIndex -1].style.display = 'block';
+}
+
+function toChangeSlide(n) {
+    showSlides((slideIndex += n));
+}
